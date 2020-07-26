a96a41a7458843df0aceb886104c46f5259f2df9
Bdiebeak <123redcreeper123@gmail.com>
2020-07-27 00:04:49 +0400

    Добавил куб на сцену и реализовал управление камерой

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform lookAtTransform;
+
+    public float zoomSpeed = 10f;
+    public float rotationSpeed = 45f;
+
+    public float minZoom = 0.5f;
+    public float maxZoom = 15f;
+
+    private Transform cameraTransform;
+
+    private void Start()
+    {
+        cameraTransform = Camera.main.transform;
+    }
+
+    private void Update()
+    {
+        Zoom();
+        Rotate();
+    }
+
+    private void Zoom()
+    {
+        float cameraZ = Mathf.Abs(cameraTransform.position.z);
+
+        if(Input.GetKey(KeyCode.Q) && cameraZ < maxZoom)
+        {
+            cameraTransform.position -= cameraTransform.forward * zoomSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.E) && cameraZ > minZoom)
+        {
+            cameraTransform.position += cameraTransform.forward * zoomSpeed * Time.deltaTime;
+        }
+    }
+
+    private void Rotate()
+    {
+        cameraTransform.LookAt(lookAtTransform);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            cameraTransform.position += cameraTransform.up * zoomSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            cameraTransform.position -= cameraTransform.right * zoomSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            cameraTransform.position -= cameraTransform.up * zoomSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            cameraTransform.position += cameraTransform.right * zoomSpeed * Time.deltaTime;
+        }       
+    }
+}
